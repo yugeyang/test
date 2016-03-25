483ad589a90e0860588b063d952e864a87b1103a
于戈洋 <geyangyu@creditease.cn>
2016-03-25 11:30:16 +0800

    提交仓库

---
+++ src/com/Test.java
@@ -0,0 +1,10 @@
+package com;
+
+import aaa.Refer;
+public class Test {
+     public static void main(String[] args) {
+		 Refer r=new Refer();
+		 r.hashCode();
+    	 System.out.println("hello");
+	}
+}
